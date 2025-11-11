411c8997fd89c55ec025407e8371743ce62302dc
tkhs-0114 <tkhs.mail0114@gmail.com>
2025-11-11 15:37:45 +0900

    Init: Init SpringBoot

---
+++ tkk_game/src/main/java/team3/tkk_game/TkkGameApplication.java
@@ -0,0 +1,13 @@
+package team3.tkk_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkkGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkkGameApplication.class, args);
+	}
+
+}
---
+++ tkk_game/src/test/java/team3/tkk_game/TkkGameApplicationTests.java
@@ -0,0 +1,13 @@
+package team3.tkk_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkkGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
