71cd12b4a8169db586bb788c870d021e4f64502e
e1b23020 <e1b23020@oit.ac.jp>
2025-12-09 15:36:01 +0900

    初期値の変更

--- tkk_game/src/main/resources/data.sql
+++ tkk_game/src/main/resources/data.sql
@@ -5,5 +5,5 @@ INSERT INTO koma VALUES (4,'銀');
 INSERT INTO koma VALUES (5,'金');
 INSERT INTO koma VALUES (6,'角');
 INSERT INTO koma VALUES (7,'飛');
-INSERT INTO koma VALUES (8,'玉');
-INSERT INTO Deck (name, sfen) VALUES ('sample', '5/5');
+INSERT INTO koma VALUES (8,'王');
+INSERT INTO Deck (name, sfen) VALUES ('sample', '5/2[8]2');
